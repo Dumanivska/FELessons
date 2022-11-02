ee3cb8b3828b1ef5b6feaa100bf730e05a726eae
Dumanivska <Yuliadumanivska@gmail.com>
2022-11-02 14:55:37 +0200

    1 js homework

---
+++ 1js/norma/js/main.js
@@ -0,0 +1,11 @@
+// 1 
+let name = prompt("Яке Ваше ім'я?");
+console.log("Привіт, " + name);
+// 2 
+let year = prompt("Який Ваш рік народження", 0);
+const now_year = 2022;
+console.log("Вам зараз " + (now_year - year));
+// 3 
+let side = prompt("Введіть довжину сторони квадрату", 0);
+let perimeter = side * 4;
+console.log("Периметр квадрату дорівнює = " + perimeter);
