a7446576e182ae755e86101f1c7aeaef1a36e64d
Dumanivska <Yuliadumanivska@gmail.com>
2022-11-12 11:01:51 +0200

    2js Homework

--- 2js/maximum/js/main.js
+++ 2js/maximum/js/main.js
@@ -4,4 +4,4 @@ let procent_Month = inputSumm * (0.05/12);
 let first_Month = (procent_Month * inputSumm) + inputSumm;
 let second_Month = (procent_Month * inputSumm) + first_Month;
 let final_Summ = second_Month + inputSumm;
-alert('Ваш рахунок через 2 месяці буде становити ' + final_Summ );
+alert('Ваш рахунок через 2 місяці буде становити ' + final_Summ );
